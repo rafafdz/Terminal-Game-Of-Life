a579ad35d6568e74d4d4f9d383cd72cfdec023ed
Rafael Fernandez <rafafdz01@gmail.com>
2018-04-11 10:10:50 -0300

    .

--- tgol.py
+++ tgol.py
@@ -2,7 +2,7 @@ import tgol.colorama as colorama
 from tgol.ventana import *
 
 
-ventana = Ventana(100, 5000, 20, "opti1")
+ventana = Ventana(110, 110 ** 2 , 60, "opti1")
 ventana.mainloop()
 
 if __name__ == "__main__":
